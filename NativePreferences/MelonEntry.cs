0895cfe28881061cb1644cde05667271f0b895c3
laughingbank <106816217+laughingbank@users.noreply.github.com>
2022-06-03 05:50:45 -0700

    Update MelonEntry.cs

--- NativePreferences/MelonEntry.cs
+++ NativePreferences/MelonEntry.cs
@@ -10,7 +10,7 @@ namespace NativePreferences
 
         public static void OnApplicationStart()
         {
-            NativeLogger.Msg("Loaded NativePreferences v6.1.1.4 by Dotlezz & Null & laughingbank");
+            NativeLogger.Msg("Loaded NativePreferences v6.1.1.4 by Dotlezz & Null");
             ReModCE.ReModCE.LoadResources();
             MelonCoroutines.Start(Instance.OnUIManager());
         }
